c032921619985af7becab998d3937dabc70157de
chaika_sv <chaika_sv@mail.ru>
2024-10-10 16:15:42 +0300

    Just added some comments

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -18,10 +18,10 @@ public class Playing extends State implements Statemethods{
     private PauseOverLay pauseOverLay;
 
     private int xLvlOffset;
-    private int leftBorder = (int) (0.2 * Game.GAME_WIDTH);     // left 20% of the visible screen. when we reach it we need to move screen
-    private int rightBorder = (int) (0.8 * Game.GAME_WIDTH);    // right 20% of the visible screen. when we reach it we need to move screen
-    private int lvlTilesWide = LoadSave.GetLevelData()[0].length;       // whole level width
-    private int maxTilesOffset = lvlTilesWide - Game.TILES_IN_WIDTH;    // whole level width minus visible screen width
+    private int leftBorder = (int) (0.2 * Game.GAME_WIDTH);     // left 20% of the visible screen (in pixels). when we reach it we need to move screen
+    private int rightBorder = (int) (0.8 * Game.GAME_WIDTH);    // right 20% of the visible screen (in pixels). when we reach it we need to move screen
+    private int lvlTilesWide = LoadSave.GetLevelData()[0].length;       // whole level width (in tiles)
+    private int maxTilesOffset = lvlTilesWide - Game.TILES_IN_WIDTH;    // whole level width minus visible screen width (in tiles). Invisible part of the level
     private int maxLvlOffsetX = maxTilesOffset * Game.TILES_SIZE;       // previous one in pixels
 
     public Playing(Game game) {
@@ -52,15 +52,50 @@ public class Playing extends State implements Statemethods{
         }
     }
 
+    /**
+     * Checking if we are close to border then we need to set some xLvlOffset to modify x coords of player and level manager
+     * Example 1:
+     *         pX = 85
+     *         offset = 0
+     *         rightBorder = 80
+     *         The player is out the border so we need to add some offset
+     *
+     *         diff = 85 - 0 = 85
+     *
+     *         if (85 - 80)     offset += 85 - 80      ---> offset += 5
+     *
+     * Example 2:
+     *         pX = 85
+     *         offset = 5
+     *         rightBorder = 80
+     *         The player is out the border, but there is already some offset so need to add more offset
+     *
+     *         diff = 85 - 5 = 80
+     *
+     *         if (80 - 80)     ---> nothing to do with offset
+     *
+     * Example 3:
+     *         pX = 30
+     *         offset = 15
+     *         leftBorder = 20
+     *         The player is NOT out the border, but we have some offset so we need to correct it
+     *
+     *         diff = 30 - 15 = 15
+     *
+     *         if (15 - 20)     offset += 15 - 20      ---> offset -= 5
+     */
     private void checkCloseToBorder() {
+
         int playerX = (int) player.getHitbox().x;
         int diff = playerX - xLvlOffset;
 
+        // Checking screen borders (if we need to move it)
         if (diff > rightBorder)
             xLvlOffset += diff - rightBorder;
         else if (diff < leftBorder)
             xLvlOffset += diff - leftBorder;
 
+        // Checking the level's borders
         if (xLvlOffset > maxLvlOffsetX)
             xLvlOffset = maxLvlOffsetX;
         else if (xLvlOffset < 0)
